3c5a693f9bb002517443b10b2665c8d13abc01bc
OK-Penalty-218 <jrchamp99@gmail.com>
2024-12-12 17:50:49 -1000

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'Homebridge MohuanLED BLE';
+export const PLATFORM_NAME = 'Homebridge Keepsmile BLE';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@sorae42/homebridge-mohuan-ble';
+export const PLUGIN_NAME = '@OK-Penalty-218/homebridge-keepsmile-ble';
